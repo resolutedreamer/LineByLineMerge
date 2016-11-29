d826f257f2adab13509ac33799040f678db8f9d4
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-11-29 01:42:33 -0800

    Prepare for TravisCI

---
+++ src/LineByLineMergeTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Tests for ConnectFour.py
+import unittest
+import sys
+
+class ConnectFourTest(unittest.TestCase):
+    def test_top_bottom_merge(self):
+        self.assertTrue(True)
+    
+    def line_by_line_merge(self):
+        self.assertTrue(True)
+    
+if __name__ == '__main__':
+    unittest.main(buffer = True)
