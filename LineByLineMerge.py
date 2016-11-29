762b9c55870ce7c8496bd1151b473f35fa1eda13
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-11-29 01:27:49 -0800

    Edited docstring and FILE_MODE

--- LineByLineMerge.py
+++ LineByLineMerge.py
@@ -1,12 +1,17 @@
 '''
-TopBottomMerge.py
-
-Can be used two ways:
-1. For the files in the same folder as this script, perform a TopBottomMerge on the files that match 'expression'
-2. For the files passed in at the commandline, perform a TopBottomMerge on those files
+LineByLineMerge.py
+FILE_MODE = 0
+For the files in the same folder as this script, perform a TopBottomMerge on the files that match 'expression'
+FILE_MODE = 1
+For the files passed in at the commandline, perform a TopBottomMerge on those files
+PROCESS_MODE = 0
+Perform a line by line merge with lines seperated by seperator
+PROCESS_MODE = 1
+Perform a top bottom merge
 '''
 import sys
 import os
+FILE_MODE = 0
 PROCESS_MODE = 0
 expression = ''
 seperator = ''
@@ -15,13 +20,21 @@ def main():
     fileHandlers = []
     fileNames = []
     foundFileNames = []
-
-    files = [f for f in os.listdir('.') if os.path.isfile(f)]
-    for f in files:
-        print f
-        if f.find(expression) != -1:
-            foundFileNames.append(f)
-    if (len(foundFileNames) == 0):
+    
+    if FILE_MODE == 0:
+        files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        for f in files:
+            print f
+            if f.find(expression) != -1:
+                foundFileNames.append(f)
+        for argument in foundFileNames:
+            try:
+                f = open(argument, 'r+')
+                fileHandlers.append(f)
+                print f.closed
+            except:
+                print "open " + argument + "failed"
+    elif FILE_MODE == 1:
         print "no files matching 'expression' found, checking passed in arguments"
         if len(sys.argv) < 2:
             print "Not enough args"
@@ -40,14 +53,6 @@ def main():
             print f.closed
         except:
             print "open " + argument + "failed"
-    else:
-        for argument in foundFileNames:
-            try:
-                f = open(argument, 'r+')
-                fileHandlers.append(f)
-                print f.closed
-            except:
-                print "open " + argument + "failed"
     if PROCESS_MODE == 0:
         line_by_line_merge(fileHandlers)
     elif PROCESS_MODE == 1:
