81883d44f7dcd3ce498d661adeb6df8b4a853c44
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-11-30 02:11:22 -0800

    Refactor
    - Added .gitignore
    - Refactored LineByLineMerge.py into a class
    - Added 2 tests, incomplete

--- src/LineByLineMerge.py
+++ src/LineByLineMerge.py
@@ -1,28 +1,25 @@
 '''
 LineByLineMerge.py
-FILE_MODE = 0
-For the files in the same folder as this script, perform a TopBottomMerge on the files that match 'expression'
-FILE_MODE = 1
-For the files passed in at the commandline, perform a TopBottomMerge on those files
-PROCESS_MODE = 0
-Perform a line by line merge with lines seperated by seperator
-PROCESS_MODE = 1
-Perform a top bottom merge
-PROCESS_MODE = 2
-Extract lines that come after lines starting with 'start'
 '''
 import sys
 import os
+
 FILE_MODE = 0
 PROCESS_MODE = 0
 expression = ''
 seperator = ''
 start = ''
 
-def main():
+
+class TxtFileProcessor:
     fileHandlers = []
-            
-    if FILE_MODE == 0:
+    output_filename = "output.txt"        
+
+    def unload(self):
+        for handler in self.fileHandlers:
+            handler.close()
+
+    def load_from_folder(self, expression):
         foundFileNames = []
         files = [f for f in os.listdir('.') if os.path.isfile(f)]
         for f in files:
@@ -32,12 +29,14 @@ def main():
         for argument in foundFileNames:
             try:
                 f = open(argument, 'r+')
-                fileHandlers.append(f)
+                self.fileHandlers.append(f)
                 print f.closed
             except:
                 print "open " + argument + "failed"
-        output_filename = 'merged_' + files[0]
-    elif FILE_MODE == 1:
+        self.output_filename = 'merged_' + files[0]
+        return len(self.fileHandlers)
+        
+    def load_from_args(self):
         fileNames = []
         if len(sys.argv) < 2:
             print "Not enough args"
@@ -52,74 +51,78 @@ def main():
         for argument in sys.argv:
             try:
                 f = open(argument, 'r+')
-                fileHandlers.append(f)
+                self.fileHandlers.append(f)
                 print f.closed
             except:
                 print "open " + argument + "failed"
-    if PROCESS_MODE == 0:
-        line_by_line_merge(fileHandlers, output_filename)
-    elif PROCESS_MODE == 1:
-        top_bottom_merge(fileHandlers, output_filename)
-    elif PROCESS_MODE == 2:
-        extract_subsequent_line(fileHandlers, output_filename)
-    return 0
-
-def extract_subsequent_line(fileHandlers, output_filename):
-    output_this = False
-    with open(output_filename, 'w+') as out:
-        print "Writing to " + output_filename
-        for handler in fileHandlers:
-            for line in handler:
-                if output_this:
+        self.output_filename = 'merged_' + sys.argv[0]
+        return len(self.fileHandlers)
+    
+    def extract_subsequent_line(self, start):
+        output_this = False
+        with open(self.output_filename, 'w+') as out:
+            print "Writing to " + self.output_filename
+            for handler in self.fileHandlers:
+                for line in handler:
+                    if output_this:
+                        out.write(line)
+                        output_this = False
+                    else:
+                        if line.startswith(start):
+                            # next line
+                            output_this = True
+        self.unload()
+        
+    
+    def top_bottom_merge(self):
+        with open(self.output_filename, 'w+') as out:
+            print "Writing to " + self.output_filename
+            for handler in self.fileHandlers:    
+                for line in handler:
                     out.write(line)
-                    output_this = False
-                else:
-                    if line.startswith(start):
-                        # next line
-                        output_this = True
+                out.write('\n\n')
+            # manually close other files when done
+        self.unload()
+    
+    def line_by_line_merge(self, seperator):
+        with open(self.output_filename, 'w+') as out:
+            print "Writing to " + self.output_filename
+            exitFlag = True
+            i = 1
+            while(exitFlag):
+                print "Appending row " + str(i)
+                for index, handler in enumerate(self.fileHandlers):
+                    # get just one row from each file
+                    currentLine = handler.readline()
+                    if not currentLine:
+                        # determined by shortest file
+                        exitFlag = False
+                        break
+                    else:
+                        # need to remove the newlines
+                        currentLine = currentLine.rstrip()
+                        print currentLine
+                        out.write(currentLine)
+                        if index == 0:
+                            out.write(seperator)
+                # add a newline to indicate done with row
+                if exitFlag:
+                    out.write('\n')
+                i += 1
+            # manually close other files when done
+            print "out of while loop"
+            self.unload()
     
 
-def top_bottom_merge(fileHandlers, output_filename):
-    with open(output_filename, 'w+') as out:
-        print "Writing to " + output_filename
-        for handler in fileHandlers:    
-            for line in handler:
-                out.write(line)
-            out.write('\n\n')
-        # manually close other files when done
-        for handler in fileHandlers:
-            handler.close()
-
-def line_by_line_merge(fileHandlers, output_filename):
-    with open(output_filename, 'w+') as out:
-        print "Writing to " + output_filename
-        exitFlag = True
-        i = 1
-        while(exitFlag):
-            print "Appending row " + str(i)
-            for index, handler in enumerate(fileHandlers):
-                # get just one row from each file
-                currentLine = handler.readline()
-                if not currentLine:
-                    # determined by shortest file
-                    exitFlag = False
-                    break
-                else:
-                    # need to remove the newlines
-                    currentLine = currentLine.rstrip()
-                    print currentLine
-                    out.write(currentLine)
-                    if index == 0:
-                        out.write(seperator)
-            # add a newline to indicate done with row
-            if exitFlag:
-                out.write('\n')
-            i += 1
-        # manually close other files when done
-        print "out of while loop"
-        for handler in fileHandlers:
-            handler.close()
-
-
 if __name__ == "__main__":
-    main()
+    myProcessor = TxtFileProcessor()
+    if FILE_MODE == 0:
+        myProcessor.load_from_folder(expression)
+    elif FILE_MODE == 1:
+        myProcessor.load_from_args()
+    if PROCESS_MODE == 0:
+        myProcessor.line_by_line_merge(seperator)
+    elif PROCESS_MODE == 1:
+        myProcessor.top_bottom_merge()
+    elif PROCESS_MODE == 2:
+        myProcessor.extract_subsequent_line(start)
--- src/LineByLineMergeTest.py
+++ src/LineByLineMergeTest.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
-# Tests for ConnectFour.py
+# Tests for LineByLineMergeTest.py
 import unittest
-import sys
+from LineByLineMerge import TxtFileProcessor
 
-class ConnectFourTest(unittest.TestCase):
+class LineByLineMergeTest(unittest.TestCase):
+    def setUp(self):
+        print "setUp"
+        self.processor = TxtFileProcessor()
+        
+    def test_load_from_folder(self):
+        self.assertEquals(self.processor.load_from_folder('.py'), 3)
+        self.processor.unload()
+
+    def test_load_from_args(self):
+        self.assertTrue(True)
+
     def test_top_bottom_merge(self):
         self.assertTrue(True)
     
-    def line_by_line_merge(self):
+    def test_line_by_line_merge(self):
         self.assertTrue(True)
     
 if __name__ == '__main__':
